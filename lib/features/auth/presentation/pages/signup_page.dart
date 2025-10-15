b1b5ce186d2ad047b0edd1e33739bfd826ae2c8f
bezawitassaye <beza1221love@gmail.com>
2025-10-15 21:05:08 +0300

    create sign up pgae

---
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
