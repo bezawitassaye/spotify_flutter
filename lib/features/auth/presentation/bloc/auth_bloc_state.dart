0dd613139dfda708e8b7a62e52a9a01bf67f1737
bezawitassaye <beza1221love@gmail.com>
2025-10-17 01:02:09 +0300

    update

--- lib/features/auth/presentation/bloc/auth_bloc_state.dart
+++ lib/features/auth/presentation/bloc/auth_bloc_state.dart
@@ -4,3 +4,10 @@ part of 'auth_bloc_bloc.dart';
 sealed class AuthBlocState {}
 
 final class AuthBlocInitial extends AuthBlocState {}
+
+
+final class AuthLoading extends AuthBlocState{}
+
+final class AuthSuccess extends AuthBlocState{}
+
+final class AuthFailure extends AuthBlocState{}
