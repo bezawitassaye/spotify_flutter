9f50013c7537efa4297b2dd76415e8a769246226
bezawitassaye <beza1221love@gmail.com>
2025-10-16 21:34:24 +0300

    update

--- lib/features/auth/data/repositories/auth_repostory_implment.dart
+++ lib/features/auth/data/repositories/auth_repostory_implment.dart
@@ -1,3 +1,17 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:spotify/core/error/failures.dart';
 import 'package:spotify/features/auth/domain/repository/auth_repository.dart';
 
-class AuthRepostoryImplment implements AuthRepository {}
+class AuthRepostoryImplment implements AuthRepository {
+  @override
+  Future<Either<Failures, String>> signInWithEmailPassword({required String email, required String password}) {
+    // TODO: implement signInWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failures, String>> signUpWithEmailPassword({required String name, required String email, required String password}) {
+    // TODO: implement signUpWithEmailPassword
+    throw UnimplementedError();
+  }
+}
