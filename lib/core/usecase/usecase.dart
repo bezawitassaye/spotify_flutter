1b63ea810ae8378ed6f4dc701b64229e3bf661b9
bezawitassaye <beza1221love@gmail.com>
2025-10-17 02:04:44 +0300

    update

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -2,6 +2,7 @@ import 'package:fpdart/fpdart.dart';
 import 'package:spotify/core/error/failures.dart';
 
 abstract interface class UseCase<SuccessType,Params> {
+  
   Future<Either<Failures,SuccessType>> call(Params params);
 
 }
