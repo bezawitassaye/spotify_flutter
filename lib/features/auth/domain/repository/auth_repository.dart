5526cfb69bf326577d893971355bef0812201a23
bezawitassaye <beza1221love@gmail.com>
2025-10-16 05:35:54 +0300

    update auth repostory

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -1,5 +1,10 @@
 import 'package:fpdart/fpdart.dart';
+import 'package:spotify/core/error/failures.dart';
 
 abstract interface class AuthRepository {
-  Either<>
+  Either<Failures,String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password
+  });
 }
