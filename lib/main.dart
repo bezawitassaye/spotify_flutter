853bccdd3c502190a3bf2d9b6d45edcd89462ada
bezawitassaye <beza1221love@gmail.com>
2025-10-17 01:37:04 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,36 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:spotify/core/secrets/app_secrets.dart';
 import 'package:spotify/core/theme/theme.dart';
+import 'package:spotify/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:spotify/features/auth/data/repositories/auth_repostory_implment.dart';
+import 'package:spotify/features/auth/domain/usecase/user_sign_up.dart';
+import 'package:spotify/features/auth/presentation/bloc/auth_bloc_bloc.dart';
 import 'package:spotify/features/auth/presentation/pages/signup_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
- final supbase = await Supabase.initialize(url:AppSecrets.supabaseUrl,anonKey:AppSecrets.anonKey );
-  runApp(const MyApp());
+  final supbase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.anonKey,
+  );
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => AuthBlocBloc(
+            userSignUp: UserSignUp(
+              AuthRepostoryImplment(
+                AuthRemoteDataSourceImplement(supbase.client),
+              ),
+            ),
+          ),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
