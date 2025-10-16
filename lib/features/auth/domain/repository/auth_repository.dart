3d28646eab69739775dc0433eb27e04a4a639bf9
bezawitassaye <beza1221love@gmail.com>
2025-10-16 05:38:37 +0300

    update

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -2,9 +2,14 @@ import 'package:fpdart/fpdart.dart';
 import 'package:spotify/core/error/failures.dart';
 
 abstract interface class AuthRepository {
-  Either<Failures,String> signUpWithEmailPassword({
+  Future<Either<Failures, String>> signUpWithEmailPassword({
     required String name,
     required String email,
-    required String password
+    required String password,
+  });
+
+  Future<Either<Failures, String>> signInWithEmailPassword({
+    required String email,
+    required String password,
   });
 }
