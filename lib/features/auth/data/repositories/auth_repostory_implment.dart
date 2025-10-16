4515ebe6b7ac3079c495cdafb2ff8928a52ad5a1
bezawitassaye <beza1221love@gmail.com>
2025-10-17 00:26:03 +0300

    update

--- lib/features/auth/data/repositories/auth_repostory_implment.dart
+++ lib/features/auth/data/repositories/auth_repostory_implment.dart
@@ -1,17 +1,38 @@
 import 'package:fpdart/fpdart.dart';
+import 'package:spotify/core/error/exceptions.dart';
 import 'package:spotify/core/error/failures.dart';
+import 'package:spotify/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:spotify/features/auth/domain/repository/auth_repository.dart';
 
 class AuthRepostoryImplment implements AuthRepository {
+  
+  final AuthRemoteDataSource remoteDataSource;
+  const AuthRepostoryImplment(this.remoteDataSource);
+
   @override
-  Future<Either<Failures, String>> signInWithEmailPassword({required String email, required String password}) {
+  Future<Either<Failures, String>> signInWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
     // TODO: implement signInWithEmailPassword
     throw UnimplementedError();
   }
 
   @override
-  Future<Either<Failures, String>> signUpWithEmailPassword({required String name, required String email, required String password}) {
-    // TODO: implement signUpWithEmailPassword
-    throw UnimplementedError();
+  Future<Either<Failures, String>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try{
+      final userId = await remoteDataSource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,);
+        
+        return right(userId);
+    } on ServerException catch(e){
+         return left(Failures(e.message));
+    }
   }
 }
