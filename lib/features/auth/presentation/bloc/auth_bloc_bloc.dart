5b3fbe46426caeb73663b16d34ebaed45eef6f98
bezawitassaye <beza1221love@gmail.com>
2025-10-17 01:08:15 +0300

    update

--- lib/features/auth/presentation/bloc/auth_bloc_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc_bloc.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotify/features/auth/domain/usecase/user_sign_up.dart';
 
 part 'auth_bloc_event.dart';
 part 'auth_bloc_state.dart';
 
 class AuthBlocBloc extends Bloc<AuthBlocEvent, AuthBlocState> {
-  AuthBlocBloc() : super(AuthBlocInitial()) {
-    on<AuthBlocEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final UserSignUp _userSignUp;
+  AuthBlocBloc({required UserSignUp userSignUp})
+    : _userSignUp = userSignUp,
+      super(AuthBlocInitial()) {
+    on<AuthSignUp>((event, emit) => {});
   }
 }
--- lib/features/auth/presentation/bloc/auth_bloc_event.dart
+++ lib/features/auth/presentation/bloc/auth_bloc_event.dart
@@ -2,3 +2,5 @@ part of 'auth_bloc_bloc.dart';
 
 @immutable
 sealed class AuthBlocEvent {}
+
+final class AuthSignUp extends AuthBlocEvent{}
