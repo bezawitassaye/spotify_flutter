81d3f92780b0434933359b8dee786e8d2c6614a9
bezawitassaye <beza1221love@gmail.com>
2025-10-19 00:06:36 +0300

    update new blog ui

--- lib/features/blog/presentation/pages/add_new_blog_page.dart
+++ lib/features/blog/presentation/pages/add_new_blog_page.dart
@@ -1,9 +1,9 @@
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:spotify/core/theme/app_pallete.dart';
 
 class NewBlog extends StatefulWidget {
-  static route() => MaterialPageRoute(
-    builder: (context)=> const NewBlog());
+  static route() => MaterialPageRoute(builder: (context) => const NewBlog());
   const NewBlog({super.key});
 
   @override
@@ -16,35 +16,34 @@ class _NewBlogState extends State<NewBlog> {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          IconButton(
-            onPressed: (){}, 
-            icon: const Icon(
-              Icons.done_rounded
-            ))
+          IconButton(onPressed: () {}, icon: const Icon(Icons.done_rounded)),
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            DottedBorder(child: Container(
-              height: 150,
-              width: double.infinity,
-              
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.folder_open,
-                    size: 40,
-                  ),
-                  SizedBox(height: 15,),
-                  Text('Select your image',
-                  style: TextStyle(fontSize: 15),)
-                ],
+            DottedBorder(
+              options: RectDottedBorderOptions(
+                dashPattern: [10, 4],
+                color: AppPallete.borderColor,
+                strokeWidth: 2,
+                padding: EdgeInsets.all(16),
               ),
-        
-            ))
+              child: Container(
+                height: 150,
+                width: double.infinity,
+
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.folder_open, size: 40),
+                    SizedBox(height: 15),
+                    Text('Select your image', style: TextStyle(fontSize: 15)),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
