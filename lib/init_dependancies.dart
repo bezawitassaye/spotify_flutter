6e4701f683e52a029ee4d55933e26da72531439c
bezawitassaye <beza1221love@gmail.com>
2025-10-18 03:35:03 +0300

    update

--- lib/init_dependancies.dart
+++ lib/init_dependancies.dart
@@ -1,11 +1,34 @@
 import 'package:get_it/get_it.dart';
 import 'package:spotify/core/secrets/app_secrets.dart';
+import 'package:spotify/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:spotify/features/auth/data/repositories/auth_repostory_implment.dart';
+import 'package:spotify/features/auth/domain/repository/auth_repository.dart';
+import 'package:spotify/features/auth/domain/usecase/user_sign_up.dart';
+import 'package:spotify/features/auth/presentation/bloc/auth_bloc_bloc.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-final serviceLocator= GetIt.instance;
+
+final serviceLocator = GetIt.instance;
 
 Future<void> initDependencies() async {
+  _initAuth();
   final supbase = await Supabase.initialize(
     url: AppSecrets.supabaseUrl,
     anonKey: AppSecrets.anonKey,
+  );
+  serviceLocator.registerLazySingleton(() => supbase.client);
+}
+
+void _initAuth() {
+  serviceLocator.registerFactory<AuthRemoteDataSource>(
+    () => AuthRemoteDataSourceImplement(serviceLocator()),
+  );
+  serviceLocator.registerFactory<AuthRepository>(
+    () => AuthRepostoryImplment(serviceLocator()),
+  );
+
+  serviceLocator.registerFactory(() => UserSignUp(serviceLocator()));
+
+  serviceLocator.registerLazySingleton(
+    () => AuthBlocBloc(userSignUp: serviceLocator()),
   );
 }
