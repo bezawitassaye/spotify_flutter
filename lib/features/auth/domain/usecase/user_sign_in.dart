ba077eb4c771464ce47f298d9808d8cf7da88e62
bezawitassaye <beza1221love@gmail.com>
2025-10-18 06:04:27 +0300

    update

---
+++ lib/features/auth/domain/usecase/user_sign_in.dart
@@ -0,0 +1,24 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:spotify/core/error/failures.dart';
+import 'package:spotify/core/usecase/usecase.dart';
+import 'package:spotify/features/auth/domain/entities/user.dart';
+import 'package:spotify/features/auth/domain/repository/auth_repository.dart';
+
+class UserLogin implements UseCase<User, UserLoginParams> {
+  final AuthRepository authRepository;
+  const UserLogin(this.authRepository);
+  @override
+  Future<Either<Failures, User>> call(UserLoginParams params) async {
+    return await authRepository.signInWithEmailPassword(
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class UserLoginParams {
+  final String email;
+  final String password;
+
+  UserLoginParams({required this.email, required this.password});
+}
