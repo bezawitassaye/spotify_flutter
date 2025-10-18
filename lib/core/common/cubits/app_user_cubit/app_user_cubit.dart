c71b67ec9e52d2caadcf3ece8873473171362b77
bezawitassaye <beza1221love@gmail.com>
2025-10-18 22:05:25 +0300

    add user statein cubit

--- lib/core/common/cubits/app_user_cubit/app_user_cubit.dart
+++ lib/core/common/cubits/app_user_cubit/app_user_cubit.dart
@@ -1,9 +1,16 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spotify/core/common/entities/user.dart';
 
 part 'app_user_state.dart';
 
 class AppUserCubit extends Cubit<AppUserState> {
   AppUserCubit() : super(AppUserInitial());
+
+  void updateUser(User? user){
+    if(user == null){
+      emit(AppUserInitial());
+    } else{
+      emit(AppUserLoggedIn(user));
+    }
+  }
 }
