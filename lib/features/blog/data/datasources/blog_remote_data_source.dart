892ace2120f3d0a423355093dcc6a69752f893ce
bezawitassaye <beza1221love@gmail.com>
2025-10-20 03:34:59 +0300

    add image upload and retrive

--- lib/features/blog/data/datasources/blog_remote_data_source.dart
+++ lib/features/blog/data/datasources/blog_remote_data_source.dart
@@ -1,26 +1,45 @@
+import 'dart:io';
 import 'package:spotify/core/error/exceptions.dart';
 import 'package:spotify/features/blog/data/models/blog_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-abstract interface class BlogRemoteDataSource{
+abstract interface class BlogRemoteDataSource {
   Future<BlogModel> uploadBlog(BlogModel blog);
+  Future<String> uploadBlogImage({
+    required File image,
+    required BlogModel blog,
+  });
 }
 
-class BlogRemoteDataSourceImplementation implements BlogRemoteDataSource{
- final SupabaseClient supabaseClient;
+class BlogRemoteDataSourceImplementation implements BlogRemoteDataSource {
+  final SupabaseClient supabaseClient;
 
   BlogRemoteDataSourceImplementation({required this.supabaseClient});
- 
+
   @override
   Future<BlogModel> uploadBlog(BlogModel blog) async {
-    try{
-      final response = await supabaseClient.from("blogs").insert(blog.toJson()).select();
+    try {
+      final response = await supabaseClient
+          .from("blogs")
+          .insert(blog.toJson())
+          .select();
 
-       return BlogModel.fromJson(response.first);  
-    }catch(e){
+      return BlogModel.fromJson(response.first);
+    } catch (e) {
       throw ServerException(e.toString());
     }
-    
   }
 
+  @override
+  Future<String> uploadBlogImage({
+    required File image,
+    required BlogModel blog,
+  }) async {
+    try {
+      await supabaseClient.storage.from("blog_images").upload(blog.id, image);
+      return supabaseClient.storage.from('blog_images').getPublicUrl(blog.id);
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
 }
