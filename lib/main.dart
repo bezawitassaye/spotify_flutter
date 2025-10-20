e2f5003606a8acf68b789b13a77b8bca93fc645d
bezawitassaye <beza1221love@gmail.com>
2025-10-20 20:51:37 +0300

    add blogblog to main dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:spotify/core/common/cubits/app_user_cubit/app_user_cubit.dart';
 import 'package:spotify/core/theme/theme.dart';
 import 'package:spotify/features/auth/presentation/bloc/auth_bloc_bloc.dart';
 import 'package:spotify/features/auth/presentation/pages/signin_page.dart';
+import 'package:spotify/features/blog/presentation/bloc/blog_bloc_bloc.dart';
 import 'package:spotify/features/blog/presentation/pages/blog_page.dart';
 import 'package:spotify/init_dependancies.dart';
 
@@ -15,6 +16,7 @@ void main() async {
       providers: [
         BlocProvider(create: (_) => serviceLocator<AppUserCubit>()),
         BlocProvider(create: (_) => serviceLocator<AuthBlocBloc>()),
+        BlocProvider(create: (_) => serviceLocator<BlogBlocBloc>()),
       ],
       child: const MyApp(),
     ),
