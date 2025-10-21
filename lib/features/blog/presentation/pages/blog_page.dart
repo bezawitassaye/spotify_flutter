d11ad89930c71938d1c7d922ac60e6bacf07f8b1
bezawitassaye <beza1221love@gmail.com>
2025-10-21 04:16:05 +0300

    update

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotify/core/common/widgets/loader.dart';
+import 'package:spotify/core/utils/show_snackbar.dart';
 import 'package:spotify/features/blog/presentation/bloc/blog_bloc_bloc.dart';
 import 'package:spotify/features/blog/presentation/pages/add_new_blog_page.dart';
 
@@ -34,6 +36,30 @@ class _BlogPageState extends State<BlogPage> {
               CupertinoIcons.add_circled
             ))
         ],
+      ),
+      body: BlocConsumer<BlogBlocBloc, BlogBlocState>(
+        listener: (context, state) {
+         if(state is BlogBlocFailure){
+          showSnackBar(context, state.error);
+         }
+        },
+        builder: (context, state) {
+          if(state is BlogBlocLoading){
+            return const Loader();
+          }
+          if(state is BlogDisplaySuccess){
+             return ListView.builder(
+              itemCount: state.blogs.length ,
+              itemBuilder: (context,index){
+                final blog = state.blogs[index];
+                return Text(blog.title);
+              }
+              );
+
+          }
+          return const SizedBox();
+         
+        },
       ),
     );
   }
