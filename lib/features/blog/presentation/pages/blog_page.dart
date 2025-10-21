94966a6cd367a0a312542271a045c36ebaa40ec4
bezawitassaye <beza1221love@gmail.com>
2025-10-21 04:02:46 +0300

    add init state for readblogs

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotify/features/blog/presentation/bloc/blog_bloc_bloc.dart';
 import 'package:spotify/features/blog/presentation/pages/add_new_blog_page.dart';
 
-class BlogPage extends StatelessWidget {
+class BlogPage extends StatefulWidget {
   static route() => MaterialPageRoute(
     builder: (context) => const BlogPage());
   const BlogPage({super.key});
 
+  @override
+  State<BlogPage> createState() => _BlogPageState();
+}
+
+class _BlogPageState extends State<BlogPage> {
+  @override
+  void initState(){
+    super.initState();
+    context.read<BlogBlocBloc>().add(BlogFetchAllBlogs());
+  
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
